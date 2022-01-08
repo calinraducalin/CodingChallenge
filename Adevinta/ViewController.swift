fa6afdb5254894aa373c87ecd64733ab7ba4ab90
Calin Radu Calin <crcalin@gmail.com>
2022-01-08 19:52:26 +0100

    Initial Commit

---
+++ Adevinta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adevinta
+//
+//  Created by Calin Radu Calin on 08.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
