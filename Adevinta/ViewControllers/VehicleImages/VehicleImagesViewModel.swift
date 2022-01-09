d5e45f69dff86c076ffd0d556c8dfae634362806
Calin Radu Calin <crcalin@gmail.com>
2022-01-09 19:17:35 +0100

    little code refactoring

--- Adevinta/ViewControllers/VehicleImages/VehicleImagesViewModel.swift
+++ Adevinta/ViewControllers/VehicleImages/VehicleImagesViewModel.swift
@@ -28,6 +28,42 @@ final class VehicleImagesViewModel {
         setupSubscriptions()
     }
 
+    func makeLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.36)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+
+        return layout
+    }
+
+    func showPageImageViewController(selectedUri: String) {
+        let imageUris = snapshot.itemIdentifiers.map { $0.uri }
+        let pageImageScreen = PageImageScreen(selectedUri: selectedUri, imageUris: imageUris)
+        presenter.show(pageImageScreen, presentation: .present, animated: true)
+    }
+
+    func showAlert(title: String?, message: String?) {
+        let alertScreen = AlertScreen(title: title, message: message)
+        presenter.show(alertScreen, presentation: .present, animated: true)
+    }
+    
+}
+
+private extension VehicleImagesViewModel {
+
     func setupSubscriptions() {
         vehicleDetailsCancellable?.cancel()
         $searchText.sink { [weak self] searchText in
@@ -60,38 +96,6 @@ final class VehicleImagesViewModel {
         vehicleDetailsCancellable?.store(in: &subscriptions)
     }
 
-    func showPageImageViewController(selectedUri: String) {
-        let imageUris = snapshot.itemIdentifiers.map { $0.uri }
-        let pageImageScreen = PageImageScreen(selectedUri: selectedUri, imageUris: imageUris)
-        presenter.show(pageImageScreen, presentation: .present, animated: true)
-    }
-
-    func showAlert(title: String?, message: String?) {
-        let alertScreen = AlertScreen(title: title, message: message)
-        presenter.show(alertScreen, presentation: .present, animated: true)
-    }
-
-    func makeLayout() -> UICollectionViewLayout {
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(0.5),
-            heightDimension: .fractionalHeight(1.0)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .fractionalWidth(0.36)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: groupSize,
-            subitems: [item]
-        )
-        let section = NSCollectionLayoutSection(group: group)
-        let layout = UICollectionViewCompositionalLayout(section: section)
-
-        return layout
-    }
-
     func makeSnapshot(from uris: [String]) -> NSDiffableDataSourceSnapshot<Int, VehicleImageItem> {
         let items = uris.map { VehicleImageItem(uri: $0) }
         var snapshot = NSDiffableDataSourceSnapshot<Int, VehicleImageItem>()
@@ -100,5 +104,5 @@ final class VehicleImagesViewModel {
 
         return snapshot
     }
-    
+
 }
